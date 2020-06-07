577e95110c5ad683aae393be375ef366de7502cb
Somya-Developer <66526723+Somya-Developer@users.noreply.github.com>
2020-06-07 21:35:41 +0530

    Add files via upload

---
+++ 2dimensional array.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int arr[2][2];
+	printf("ENTER THE VALUE OF [0][0]\n");
+	scanf("%d",&arr[0][0]);
+	printf("ENTER THE VALUE OF [0][1]\n");
+	scanf("%d",&arr[0][1]);
+	printf("ENTER THE VALUE OF [1][0]\n");
+	scanf("%d",&arr[1][0]);	
+	printf("ENTER THE VALUE OF [1][1]\n");
+	scanf("%d",&arr[1][1]);	
+	printf("THIS ARE YOUR INPUTS...\n");
+	printf("%d\n",arr[0][0]);
+	printf("%d\n",arr[0][1]);
+	printf("%d\n",arr[1][0]);
+	printf("%d\n",arr[1][1]);
+}
+//
